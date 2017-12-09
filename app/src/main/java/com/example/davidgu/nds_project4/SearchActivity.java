682fed0f7fe44a45bcc4fddef01ae0c193fe0ee0
guzheyuan <guzheyuan@datebao.com>
2017-12-08 19:01:49 -0500

    hardcoded

--- app/src/main/java/com/example/davidgu/nds_project4/SearchActivity.java
+++ app/src/main/java/com/example/davidgu/nds_project4/SearchActivity.java
@@ -3,6 +3,7 @@ package com.example.davidgu.nds_project4;
         import android.content.Intent;
         import android.graphics.Bitmap;
         import android.graphics.BitmapFactory;
+        import android.provider.MediaStore;
         import android.support.annotation.NonNull;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
@@ -33,7 +34,9 @@ package com.example.davidgu.nds_project4;
         import android.net.Uri;
 
         import java.io.File;
+        import java.io.FileNotFoundException;
         import java.io.IOException;
+        import java.io.InputStream;
         import java.util.UUID;
 
 
@@ -42,7 +45,7 @@ public class SearchActivity extends AppCompatActivity {
     //
 //    private FirebaseStorage storage = FirebaseStorage.getInstance();
 //    private StorageReference storageRef = storage.getReferenceFromUrl("gs://mockinstagram-7a1fe.appspot.com").child("firememes/0b13a432-312e-4b3e-8207-dbcff85ec2ee.png");
-    private DatabaseReference userDatabase = FirebaseDatabase.getInstance().getReference("user");
+    private DatabaseReference userDatabase = FirebaseDatabase.getInstance().getReference();
     private String email;
 
     EditText editText;
@@ -89,23 +92,36 @@ public class SearchActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Log.d(TAG, "in onClick");
-                String description = editText.getText().toString();
-                userDatabase.child("Discription").equalTo(description).addValueEventListener(new ValueEventListener() {
+                Uri url = Uri.parse("https://firebasestorage.googleapis.com/v0/b/mockinstagram-7a1fe.appspot.com/o/firememes%2Fd3363923-16c0-4a36-8e83-e7a3f0b4b321.png?alt=media&token=52aa299b-d13e-41f9-9585-42608e4c35f9");
 
-                    @Override
-                    public void onDataChange(DataSnapshot snapshot) {
-                        Log.d(TAG, "in onDataChange");
-                        User user = snapshot.getValue(User.class);
-                        Log.d(TAG, user.img_Url);
-                    }
+                InputStream image_stream = null;
+                try {
+                    image_stream = getContentResolver().openInputStream(url);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                Bitmap bitmap= BitmapFactory.decodeStream(image_stream );
+                mImageView.setImageBitmap(bitmap);
+                //
+//                Bitmap bitmap;
+//                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), url);
+//                mImageView.setImageBitmap(bitmap);
 
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-
-                    }
-
-
-                });
+//                String description = editText.getText().toString();
+//                userDatabase.child("mockinstagram-7a1fe").child("Discription").equalTo(description).addValueEventListener(new ValueEventListener() {
+//
+//                    @Override
+//                    public void onDataChange(DataSnapshot snapshot) {
+//                        Log.d(TAG, "in onDataChange");
+//                        User user = snapshot.getValue(User.class);
+//                        Log.d(TAG, user.img_Url);
+//                    }
+//
+//                    @Override
+//                    public void onCancelled(DatabaseError databaseError) {
+//
+//                    }
+//                });
             }
         });
 
